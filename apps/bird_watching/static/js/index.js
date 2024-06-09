476b04f2283b0d49254660a7edf5640402ebd6b9
Danny Nguyen <dnguy206@ucsc.edu>
2024-06-08 20:40:18 -0700

    added functionality to index.js for submitting checklist and my birding page linking as well as specifying species

--- apps/bird_watching/static/js/index.js
+++ apps/bird_watching/static/js/index.js
@@ -1,33 +1,41 @@
-"use strict";
-
-// This will be the object that will contain the Vue attributes
-// and be used to initialize it.
-let app = {};
-
-
-app.data = {    
-    data: function() {
-        return {
-            // Complete as you see fit.
-            my_value: 1, // This is an example.
-        };
+new Vue({
+    el: '#app',
+    data: {
+        my_value: 'Welcome to the Bird Watching App!',
+        selectedSpecies: 'all',
+        speciesList: ['Sparrow', 'Eagle', 'Hawk', 'Owl'],
+        map: null,
+        heatmapLayer: null
+    },
+    mounted() {
+        this.initMap();
     },
     methods: {
-        // Complete as you see fit.
-        my_function: function() {
-            // This is an example.
-            this.my_value += 1;
+        initMap() {
+            this.map = L.map('map').setView([51.505, -0.09], 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(this.map);
+
+            this.heatmapLayer = L.heatLayer([], { radius: 25 }).addTo(this.map);
+            this.updateMap();
         },
+        updateMap() {
+            // Fetch data based on selectedSpecies and update heatmapLayer
+            // For now, let's add some dummy data
+            const dummyData = [
+                [51.505, -0.09, 0.5],
+                [51.51, -0.1, 0.5],
+                [51.51, -0.12, 0.5]
+            ];
+            this.heatmapLayer.setLatLngs(dummyData);
+        },
+        navigateToChecklist() {
+            window.location.href = '/checklist';
+        },
+        navigateToStats() {
+            window.location.href = '/stats';
+        }
     }
-};
-
-app.vue = Vue.createApp(app.data).mount("#app");
-
-app.load_data = function () {
-    axios.get(my_callback_url).then(function (r) {
-        app.vue.my_value = r.data.my_value;
-    });
-}
-
-app.load_data();
-
+});
